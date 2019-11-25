66291cbac7751d317c47ddf27e18255a742f5b5b
Aquarian-Age <xiaoyaoke7630@gmail.com>
2019-11-25 09:47:58 +0800

    指定编译后生成名字

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -v
-CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build main.go
-CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build main.go
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build main.go 
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o windows.exe main.go
+CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o darwin main.go
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o linux main.go 
 
