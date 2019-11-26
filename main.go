77c37b5c6037ca07d021d129949f51d681b9a071
Aquarian-Age <xiaoyaoke7630@gmail.com>
2019-11-26 19:19:02 +0800

    添加了六特性神

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"mingfajuan/public"
 	"mingfajuan/qike"
 )
 
 func main()  {
 	qike.QiKe()
+	public.Choise()
 	qike.About()
 }
 
---
+++ public/input.go
@@ -0,0 +1,27 @@
+package public
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+//获取输入信息
+
+func Get_In_Put()string  {
+	input := bufio.NewReader(os.Stdin)
+	input_s,err := input.ReadString('\n')
+	if err != nil{
+		fmt.Println(err)
+	}
+	s := strings.ToLower(strings.TrimSpace(input_s))
+	return s
+}
+
+func Choise()  {
+	fmt.Println("查看六神属性按p")
+	if Get_In_Put() =="p" {
+		Liu_Shen() //六神属性
+	}
+}
